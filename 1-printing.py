497f68b980c0596a369d4a5f3143840f561ce930
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-04 11:42:07 -0300

    More organization

--- 1-printing.py
+++
@@ -1,7 +0,0 @@
-"""
-    Write a program that prints
-    something in the terminal
-"""
-
-# single quotes to avoid closing the string
-print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
