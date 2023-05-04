96565563c6a68ba145ce1e246f343cbc5b86ef80
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-04 07:16:29 -0700

    Comments

--- 2-Debugging.py
+++ 2-Debugging.py
@@ -5,8 +5,14 @@
         print('e.g. print("Hello " + "world")')
     print(("New lines can be created with a backslash and n.")
 """
-
+# Missing double quotes
 print("Day 1 - String Manipulation")
+
+# Single quotes instead of doubles
 print('String Concatenation is done with the "+" sign.')
+
+# Extra indentation removed
 print('e.g. print("Hello " + "world")')
+
+# Extra ( removed
 print("New lines can be created with a backslash and n.")
