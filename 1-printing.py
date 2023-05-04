17db106095992ab83532e25e23aebecc3dac7623
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-04 06:47:35 -0700

    Exercise 1

---
+++ 1-printing.py
@@ -0,0 +1,8 @@
+"""
+    Write a program that prints
+    something in the terminal
+"""
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")  # single quotes to avoid closing the string
