0624f09f3c7df3561defc3556fa08ba5f48868e7
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-04 08:47:10 -0700

    Final project

---
+++ Day-1_beginner/Band_name_generator.py
@@ -0,0 +1,15 @@
+"""
+    Band name generator project
+"""
+
+# Greeting
+print("Welcome to the band name generator.")
+
+# Asks for the user's city
+city = input("Which city did you grow up in?\n")
+
+# Asks for the user's pet
+pet = input("What is the name of a pet?\n")
+
+# Combine the name of the cityand the pet and show it
+print("Your band name could be " + city + " " + pet)
