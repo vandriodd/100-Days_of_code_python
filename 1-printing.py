70f3424e1c6a097aba4aacfaca06a88b1e77adf9
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-04 06:52:54 -0700

    another way to do exercise 1

--- 1-printing.py
+++ 1-printing.py
@@ -3,6 +3,5 @@
     something in the terminal
 """
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")  # single quotes to avoid closing the string
+# single quotes to avoid closing the string
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
