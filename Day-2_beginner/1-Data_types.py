3ee1fae97f4804366d70e68498b884dd752c5643
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-16 11:17:53 -0700

    Data types

---
+++ Day-2_beginner/1-Data_types.py
@@ -0,0 +1,12 @@
+"""
+    Write a program that adds
+    the digits in a 2 digit number
+"""
+
+two_digit_number = input("Type a two digit number: ")
+
+# Change data type
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+
+print(num1 + num2)
